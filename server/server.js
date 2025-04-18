57502dc751c74448fa43e7b19db9e9b0462140df
Nick Wade <nick.wade183@gmail.com>
2025-04-18 12:59:25 +1000

    updated server.js so the data is served from json

--- server/server.js
+++ server/server.js
@@ -7,7 +7,27 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8080;
 
+const readJSONFile = (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading JSON file:', error);
+        return null;
+    }
+};
+
 app.get('/api/iga', async (req, res) => {
+    const igaDataPath = path.join(__dirname, 'data', 'iga.json');
+    
+    // Check if JSON file exists
+    if (fs.existsSync(igaDataPath)) {
+        const data = readJSONFile(igaDataPath);
+        if (data) {
+            return res.json(data);
+        }
+    }
+
     try {
         const data = await scrapeIGA();
         res.json(data);
@@ -17,12 +37,22 @@ app.get('/api/iga', async (req, res) => {
 });
 
 app.get('/api/aldi', async (req, res) => {
+    const aldiDataPath = path.join(__dirname, 'data', 'ALDI.json');
+
+    // Check if JSON file exists
+    if (fs.existsSync(aldiDataPath)) {
+        const data = readJSONFile(aldiDataPath);
+        if (data) {
+            return res.json(data);
+        }
+    }
+
+
     try {
         const data = await scrapeALDI();
         res.json(data);
     } catch (error) {
         res.status(500).json({ message: 'Error scraping ALDI data', error });
     }
-});
-scrapeALDI();
+});peALDI();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
